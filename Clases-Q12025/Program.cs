bae57b6d353374f3fd8fb4705ccf4d8b944dd919
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-22 19:12:32 -0600

    Adding data

--- Clases-Q12025/Program.cs
+++ Clases-Q12025/Program.cs
@@ -13,3 +13,14 @@ ave.Familia.Nombre = "Psittacidae";
 ave.Familia.NumeroEspecie = 393;
 ave.Familia.Caracteristicas = "Aves coloridas, a menudo inteligentes";
 ave.Imprimir();
+
+Ave ave2 = new Ave();
+ave2.Nombre = "Flamenco";
+ave2.Especie = "Phoenicopterus roseus";
+ave2.Color = "Rosa";
+ave2.Tamano = 120;
+ave2.Habitat = "Lagunas Salinas";
+ave2.Familia.Nombre = "Phoenicopteridae";
+ave2.Familia.NumeroEspecie = 6;
+ave2.Familia.Caracteristicas = "Aves zancudas con patas largas y cuellos largos.";
+ave2.Imprimir();
