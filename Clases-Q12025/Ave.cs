aa9946a0a1e93b5ced78e84cba7f1542b49723fd
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-22 18:56:38 -0600

    Create Ave class

---
+++ Clases-Q12025/Ave.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Clases_Q12025
+{
+    public class Ave
+    {
+        public string? Nombre { get; set; }
+        public string? Especie { get; set; }
+        public string? Color { get; set; }
+        public double Tamano { get; set; }
+        public string? Habitat { get; set; }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Especie: {Especie}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Tamaño: {Tamano} cm");
+            Console.WriteLine($"Habitat: {Habitat}");
+        }
+    }
+}
