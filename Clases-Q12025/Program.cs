a431bd22ba4517730e54283903ef3f6138dc4af4
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-22 18:57:06 -0600

    Adding Ave data

--- Clases-Q12025/Program.cs
+++ Clases-Q12025/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Clases_Q12025;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Drawing;
+
+Ave ave = new Ave();
+ave.Nombre = "Loro";
+ave.Especie = "Amazona aestiva";
+ave.Color = "Verde";
+ave.Tamano = 30.5;
+ave.Habitat = "Bosques Tropicales";
+ave.Imprimir();
